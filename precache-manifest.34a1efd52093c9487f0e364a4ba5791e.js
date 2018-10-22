c61899afba2532df1f6f89aafad215ef72ee039b
Dan Lewis <danl@jsbbq.local>
2018-10-22 13:52:48 -0500

    Updates

---
+++ precache-manifest.34a1efd52093c9487f0e364a4ba5791e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "94391814868a69785aeb",
+    "url": "/contactmanager/static/js/main.94391814.chunk.js"
+  },
+  {
+    "revision": "f00bc4873262a4590142",
+    "url": "/contactmanager/static/js/1.f00bc487.chunk.js"
+  },
+  {
+    "revision": "94391814868a69785aeb",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "f00bc4873262a4590142",
+    "url": "/contactmanager/static/css/1.3b98a28a.chunk.css"
+  },
+  {
+    "revision": "05f2fcaefc683503c0522aebf7d88271",
+    "url": "/contactmanager/index.html"
+  }
+];
